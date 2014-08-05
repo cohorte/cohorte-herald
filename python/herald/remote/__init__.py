e725e9bb8c033f2fbf78881541214de4939f3bb9
Thomas Calmant <thomas.calmant@gmail.com>
2014-08-05 13:09:37 +0200

    Added an Herald RS importer/exporter, based on xmlrpclib

--- python/herald/remote/__init__.py
+++ python/herald/remote/__init__.py
@@ -4,7 +4,17 @@
 Pelix Remote Services implementation based on Herald
 """
 
-FACTORY_DISCOVERY = "herald-remote-discovery-factory"
+FACTORY_DISCOVERY = 'herald-remote-discovery-factory'
 """
 Name of the Herald Remote Services discovery component factory
+"""
+
+FACTORY_HERALD_XMLRPC_EXPORTER = 'herald-rpc-exporter-xmlrpc-factory'
+"""
+Name of the component factory for the xmlrpclib-based exporter
+"""
+
+FACTORY_HERALD_XMLRPC_IMPORTER = 'herald-rpc-importer-xmlrpc-factory'
+"""
+Name of the component factory for the xmlrpclib-based importer
 """
