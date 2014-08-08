2c34994f5b533e960bec888d5725139d8d3c75cb
Thomas Calmant <thomas.calmant@gmail.com>
2014-08-08 11:51:56 +0200

    Herald RS Discovery is now a directory listener
    It now sends a 'contact' message when a new peer is registered, instead
    of sending a 'discovery' message at validation.
    => The discovery message was not sent, as nobody was yet known by
    Herald.
    
    Now we let Herald transports layer discover/register peers first.

--- python/herald/remote/discovery.py
+++ python/herald/remote/discovery.py
@@ -10,7 +10,7 @@ import herald.remote
 
 # Pelix
 from pelix.ipopo.decorators import ComponentFactory, Requires, Provides, \
-    Property, Validate, Instantiate
+    Property, Instantiate
 import pelix.remote.beans
 
 # Standard library
@@ -25,7 +25,7 @@ _logger = logging.getLogger(__name__)
 
 @ComponentFactory(herald.remote.FACTORY_DISCOVERY)
 @Provides(pelix.remote.SERVICE_EXPORT_ENDPOINT_LISTENER)
-@Provides(herald.SERVICE_LISTENER)
+@Provides((herald.SERVICE_LISTENER, herald.SERVICE_DIRECTORY_LISTENER))
 @Requires('_directory', herald.SERVICE_DIRECTORY)
 @Requires('_herald', herald.SERVICE_HERALD)
 @Requires('_dispatcher', pelix.remote.SERVICE_DISPATCHER)
@@ -43,6 +43,9 @@ class HeraldDiscovery(object):
         # Herald
         self._herald = None
         self._directory = None
+
+        # Herald messages filters
+        self._filters = []
 
         # Pelix Remote Services
         self._dispatcher = None
@@ -109,21 +112,12 @@ class HeraldDiscovery(object):
         self._herald.fire_group('all', beans.Message(self.__subject(kind),
                                                      content))
 
-    @Validate
-    def _validate(self, context):
-        """
-        Component validated
-        """
-        # Send a discovery signal
-        endpoints = self._dump_endpoints(self._dispatcher.get_endpoints())
-        self.__send_message('discovery', endpoints)
-
     def herald_message(self, herald_svc, message):
         """
         An Herald message has been received
         """
         kind = message.subject.rsplit('/', 1)[1]
-        if kind in ('add', 'discovered'):
+        if kind in ('add', 'contact'):
             # Register the new endpoints
             for endpoint_dict in message.content:
                 try:
@@ -132,6 +126,12 @@ class HeraldDiscovery(object):
                 except KeyError as ex:
                     _logger.error("Unreadable endpoint from %s: missing %s",
                                   message.sender, ex)
+
+            if kind == 'contact':
+                # Reply with the whole list of our exported endpoints
+                endpoints = \
+                    self._dump_endpoints(self._dispatcher.get_endpoints())
+                herald_svc.reply(message, endpoints, self.__subject("add"))
 
         elif kind == 'remove':
             # Message only contains the UID of the endpoint
@@ -143,10 +143,29 @@ class HeraldDiscovery(object):
             new_properties = message.content['properties']
             self._registry.update(endpoint_uid, new_properties)
 
-        elif kind == 'discovery':
-            # Reply with the whole list of our exported endpoints
-            endpoints = self._dump_endpoints(self._dispatcher.get_endpoints())
-            herald_svc.reply(message, endpoints, self.__subject("discovered"))
+    def peer_registered(self, peer):
+        """
+        A new peer has been registered in Herald: send it a contact information
+
+        :param peer: The new peer
+        """
+        # Send a contact message, with our list of endpoints
+        self.__send_message(
+            'contact', self._dump_endpoints(self._dispatcher.get_endpoints()))
+
+    def peer_updated(self, peer, access_id, data, previous):
+        """
+        An access to a peer have been updated: ignore
+        """
+        pass
+
+    def peer_unregistered(self, peer):
+        """
+        All accesses to a peer have been lost: forget about it
+
+        :param peer: The lost peer
+        """
+        self._registry.lost_framework(peer.uid)
 
     def endpoints_added(self, endpoints):
         """
