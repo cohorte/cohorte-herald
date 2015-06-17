dd975f010ab10338e824a28a2280df7f750bdf34
Thomas Calmant <thomas.calmant@isandlatech.com>
2015-06-17 15:00:21 +0200

    Remote Shell: handle unregistered peers

--- python/herald/rshell.py
+++ python/herald/rshell.py
@@ -47,6 +47,7 @@ import pelix.shell.beans
 
 # Standard library
 import logging
+import threading
 import uuid
 
 try:
@@ -77,28 +78,35 @@ MSG_CLIENT_CLOSE = "{0}/close".format(_PREFIX_CLIENT)
 SESSION_SHELL_RUNNING = "__herald_shell_running__"
 SESSION_SESSION_ID = "__session_id__"
 SESSION_CLIENT_ID = "__client_uid__"
+SESSION_SERVER_ID = "__server_uid__"
 
 # ------------------------------------------------------------------------------
 
 
 @ComponentFactory("herald-remote-shell-client-factory")
 @Requires("_herald", herald.SERVICE_HERALD)
-@Provides((pelix.shell.SERVICE_SHELL_COMMAND, herald.SERVICE_LISTENER))
+@Provides((pelix.shell.SERVICE_SHELL_COMMAND, herald.SERVICE_LISTENER,
+           herald.SERVICE_DIRECTORY_LISTENER))
 @Property('_filters', herald.PROP_FILTERS, ['{0}/*'.format(_PREFIX_CLIENT)])
 @Instantiate("herald-remote-shell-client")
 class HeraldRemoteShellClient(object):
     """
     Herald remote shell command
-
-    TODO: close session on peer unregistration
     """
     def __init__(self):
         """
         Sets up the object
         """
         self._herald = None
-        # Activate session ID => ShellSession
+
+        # Thread safety
+        self.__lock = threading.Lock()
+
+        # Active session ID -> local shell session
         self._sessions = {}
+
+        # Peer UID -> session ID
+        self._peers = {}
 
     @staticmethod
     def get_namespace():
@@ -135,14 +143,17 @@ class HeraldRemoteShellClient(object):
             session.write_line("Connection refused")
             return
 
-        prompt_str = "({0}) {1}".format(
-            shell_info['peer_uid'], shell_info['ps1'])
+        peer_uid = shell_info['peer_uid']
+        prompt_str = "({0}) {1}".format(peer_uid, shell_info['ps1'])
 
         # Setup the session variables
         session.set(SESSION_SHELL_RUNNING, True)
+        session.set(SESSION_SERVER_ID, peer_uid)
 
-        # Store the session
-        self._sessions[session_id] = session
+        with self.__lock:
+            # Store the session
+            self._sessions[session_id] = session
+            self._peers.setdefault(peer_uid, set()).add(session_id)
 
         # Print the banner
         session.write_line(shell_info['banner'])
@@ -159,32 +170,58 @@ class HeraldRemoteShellClient(object):
                     # Empty line
                     continue
 
-                # Prepare arguments
-                content = {"line": line, "session_id": session_id}
+                # Send the command line
+                content = {"session_id": session_id,
+                           "line": line}
                 result_msg = self._herald.send(
                     peer, beans.Message(MSG_SERVER_COMMAND, content))
+
+                # Print results
                 session.write_line(prompt_str, result_msg.content)
+
         except (HeraldException, KeyError) as ex:
             # Error sending message
             _logger.exception("Error sending command to peer: %s", ex)
             session.write_line("Error sending command to peer: {0}", ex)
+
         except (KeyboardInterrupt, EOFError):
-            # Ctrl+C or Ctrl+D: stop the shell
+            # Ctrl+C or Ctrl+D: just stop the shell
             pass
-        finally:
+
+        try:
+            # Clear local references
+            self.__clear_session(session_id)
+        except KeyError:
+            # Session has been closed on the server side
+            pass
+        else:
             try:
-                # Clear references
-                del self._sessions[session_id]
-            except KeyError:
-                # Session has been closed on the server side
-                pass
-            else:
-                try:
-                    # Close the session (don't worry about errors)
-                    self._herald.fire(
-                        peer, beans.Message(MSG_SERVER_CLOSE, session_id))
-                except Exception as ex:
-                    _logger.warning("Error closing session: %s", ex)
+                # Close the session (don't worry about errors)
+                self._herald.fire(
+                    peer, beans.Message(MSG_SERVER_CLOSE, session_id))
+            except Exception as ex:
+                _logger.warning("Error closing session: %s", ex)
+
+    def __clear_session(self, session_id):
+        """
+        Removes references to the given session ID
+
+        :param session_id: A session ID
+        :return: The local shell session
+        :raise KeyError: Unknown session UID
+        """
+        with self.__lock:
+            # Pop local session
+            session = self._sessions.pop(session_id)
+
+            # Clean up peer reference
+            peer_uid = session.get(SESSION_SERVER_ID)
+            peer_session = self._peers[peer_uid]
+            peer_session.discard(session_id)
+            if not peer_session:
+                del self._peers[peer_uid]
+
+        return session
 
     def herald_message(self, herald_svc, message):
         """
@@ -217,14 +254,16 @@ class HeraldRemoteShellClient(object):
                     herald_svc.reply(message, line)
                 except (EOFError, KeyboardInterrupt):
                     # Ctrl+D received: close the session
+                    if session.get(SESSION_SHELL_RUNNING):
+                        herald_svc.reply(message, session_id, MSG_SERVER_CLOSE)
+
                     session.set(SESSION_SHELL_RUNNING, False)
-                    herald_svc.reply(message, None, MSG_SERVER_CLOSE)
 
         elif kind == "close":
             # Clean up a session
             session_id = message.content
             try:
-                session = self._sessions.pop(session_id)
+                session = self.__clear_session(session_id)
                 session.write_line("Session closed. Press Enter to exit.")
                 session.set(SESSION_SHELL_RUNNING, False)
             except KeyError:
@@ -232,6 +271,45 @@ class HeraldRemoteShellClient(object):
 
         elif kind == "error":
             _logger.warning("Error on shell server side: %s", message.content)
+
+    def peer_unregistered(self, peer):
+        """
+        Peer unregistered: close local session
+        """
+        with self.__lock:
+            try:
+                # Get all active sessions for this peer
+                sessions_ids = self._peers.pop(peer.uid)
+            except KeyError:
+                # No active session: ignore
+                return
+
+            for session_id in sessions_ids:
+                try:
+                    # Pop the session & stop the loop
+                    session = self._sessions.pop(session_id)
+                    session.set(SESSION_SHELL_RUNNING, False)
+
+                    # Print an exit message
+                    session.write_line("Peer has gone away. "
+                                       "Press Enter to exit.")
+                except KeyError:
+                    # Unknown session
+                    pass
+
+    @staticmethod
+    def peer_registered(peer):
+        """
+        Peer registered: ignore
+        """
+        pass
+
+    @staticmethod
+    def peer_updated(peer, access_id, data, previous):
+        """
+        Peer updated: ignore
+        """
+        pass
 
 # ------------------------------------------------------------------------------
 
@@ -295,7 +373,7 @@ class _HeraldInputStream(object):
         content = {"session_id": self._session}
         prompt_msg = self._herald.send(
             self._peer, beans.Message(MSG_CLIENT_PROMPT, content))
-        if prompt_msg.content is None:
+        if prompt_msg.subject == MSG_SERVER_CLOSE:
             # Client closed its shell
             raise EOFError
 
@@ -306,14 +384,12 @@ class _HeraldInputStream(object):
 @Requires("_directory", herald.SERVICE_DIRECTORY)
 @Requires("_herald", herald.SERVICE_HERALD)
 @Requires("_shell", pelix.shell.SERVICE_SHELL)
-@Provides(herald.SERVICE_LISTENER)
+@Provides((herald.SERVICE_LISTENER, herald.SERVICE_DIRECTORY_LISTENER))
 @Property('_filters', herald.PROP_FILTERS, ['{0}/*'.format(_PREFIX_SERVER)])
 @Instantiate("herald-remote-shell-server")
 class HeraldRemoteShellServer(object):
     """
     Herald remote shell server
-
-    TODO: close session on peer unregistration
     """
     def __init__(self):
         """
@@ -321,13 +397,15 @@ class HeraldRemoteShellServer(object):
         """
         # Injected services
         self._directory = None
+        self._herald = None
         self._shell = None
-
-        # Activate session ID => ShellSession
-        self._sessions = {}
 
         # Local peer UID
         self._local_uid = None
+
+        # Active session ID => local shell session
+        self._sessions = {}
+        self.__lock = threading.Lock()
 
     @Validate
     def validate(self, _):
@@ -341,22 +419,23 @@ class HeraldRemoteShellServer(object):
         """
         Component invalidated
         """
-        for session in self._sessions.values():
-            peer_uid = session.get(SESSION_CLIENT_ID)
-            session_id = session.get(SESSION_SESSION_ID)
+        with self.__lock:
+            for session in self._sessions.values():
+                peer_uid = session.get(SESSION_CLIENT_ID)
+                session_id = session.get(SESSION_SESSION_ID)
 
-            try:
-                self._herald.fire(
-                    peer_uid, beans.Message(MSG_CLIENT_CLOSE, session_id))
-            except HeraldException as ex:
-                # Error sending message
-                _logger.error("Error sending session close message: %s", ex)
-            except KeyError:
-                # Unknown peer: ignore
-                pass
+                try:
+                    self._herald.fire(
+                        peer_uid, beans.Message(MSG_CLIENT_CLOSE, session_id))
+                except HeraldException as ex:
+                    # Error sending message
+                    _logger.error("Error sending session close message: %s", ex)
+                except KeyError:
+                    # Unknown peer: ignore
+                    pass
 
-        # Clear session
-        self._sessions.clear()
+            # Clear sessions
+            self._sessions.clear()
 
     def herald_message(self, herald_svc, message):
         """
@@ -380,6 +459,35 @@ class HeraldRemoteShellServer(object):
                 message, "Error handling message: {0}".format(ex),
                 MSG_CLIENT_ERROR)
 
+    def peer_unregistered(self, peer):
+        """
+        Peer unregistered: close its sessions
+        """
+        lost_peer = peer.uid
+        with self.__lock:
+            # List IDs of lost sessions
+            lost_sessions = [session_id
+                             for session_id, session in self._sessions.items()
+                             if session.get(SESSION_CLIENT_ID) == lost_peer]
+
+            # Clear them
+            for session_id in lost_sessions:
+                del self._sessions[session_id]
+
+    @staticmethod
+    def peer_registered(peer):
+        """
+        Peer registered: ignore
+        """
+        pass
+
+    @staticmethod
+    def peer_updated(peer, access_id, data, previous):
+        """
+        Peer updated: ignore
+        """
+        pass
+
     def _open_session(self, herald_svc, message):
         """
         Opens a remote shell session.
@@ -402,8 +510,9 @@ class HeraldRemoteShellServer(object):
         session.set(SESSION_SESSION_ID, session_id)
         session.set(SESSION_CLIENT_ID, message.sender)
 
-        # Store it
-        self._sessions[session_id] = session
+        with self.__lock:
+            # Store it
+            self._sessions[session_id] = session
 
         # Reply content
         content = {"session_id": session_id,
@@ -424,7 +533,8 @@ class HeraldRemoteShellServer(object):
         # Find the session
         session_id = message.content['session_id']
         try:
-            session = self._sessions[session_id]
+            with self.__lock:
+                session = self._sessions[session_id]
         except KeyError:
             # Unknown session
             _logger.warning("Unknown session for execution: %s", session_id)
@@ -446,7 +556,8 @@ class HeraldRemoteShellServer(object):
         """
         session_id = message.content
         try:
-            del self._sessions[session_id]
+            with self.__lock:
+                del self._sessions[session_id]
         except KeyError:
             # Unknown session
             _logger.warning("Unknown session to close: %s", session_id)
